b0d5eb77c36c6077140f79ebe795e1148092751c
KylieAndrea <kylieguzmanh@gmail.com>
2025-02-19 17:20:00 -0500

    .

--- db/migrate/20250219205102_create_arrozs.rb
+++ db/migrate/20250219205102_create_arrozs.rb
@@ -3,6 +3,7 @@ class CreateArrozs < ActiveRecord::Migration[8.0]
     create_table :arrozs do |t|
       t.string :marca
       t.integer :cantidad
+      t.integer :precio
 
       t.timestamps
     end
